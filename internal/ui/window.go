96f038d072875333fb376fab2cabc085b27b26bf
WillyHetland <willeyh@gmail.com>
2026-09-15 12:22:07 +0200

    update time

--- internal/ui/window.go
+++ internal/ui/window.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -13,45 +14,69 @@ import (
 func Show(app fyne.App) {
 	window := app.NewWindow("Sway Power")
 
-	bat, err := battery.Read()
+	title := widget.NewLabel("Battery")
+	title.Alignment = fyne.TextAlignCenter
 
-	if err != nil {
-		window.SetContent(
-			container.NewCenter(
-				widget.NewLabel(fmt.Sprintf("Battery error: %v", err)),
-			),
-		)
-	} else if bat == nil {
-		window.SetContent(
-			container.NewCenter(
-				widget.NewLabel("No battery detected"),
-			),
-		)
-	} else {
-		title := widget.NewLabel("Battery")
-		title.Alignment = fyne.TextAlignCenter
+	percentage := widget.NewLabel("")
+	percentage.Alignment = fyne.TextAlignCenter
 
-		percentage := widget.NewLabel(
-			fmt.Sprintf("%d%%", bat.Percentage),
-		)
-		percentage.Alignment = fyne.TextAlignCenter
+	status := widget.NewLabel("")
+	status.Alignment = fyne.TextAlignCenter
 
-		status := widget.NewLabel(
-			string(bat.Status),
-		)
-		status.Alignment = fyne.TextAlignCenter
+	progress := widget.NewProgressBar()
 
-		content := container.NewVBox(
-			title,
-			percentage,
-			status,
-		)
+	content := container.NewVBox(
+		title,
+		percentage,
+		progress,
+		status,
+	)
 
-		window.SetContent(
-			container.NewCenter(content),
-		)
-	}
+	window.SetContent(
+		container.NewCenter(content),
+	)
 
 	window.Resize(fyne.NewSize(400, 200))
+
+	// Update the UI with battery information.
+	updateUI := func(bat *battery.Battery, err error) {
+		if err != nil {
+			percentage.SetText("Error")
+			status.SetText(err.Error())
+			progress.SetValue(0)
+			return
+		}
+
+		if bat == nil {
+			percentage.SetText("No battery")
+			status.SetText("")
+			progress.SetValue(0)
+			return
+		}
+
+		percentage.SetText(fmt.Sprintf("%d%%", bat.Percentage))
+		status.SetText(string(bat.Status))
+		progress.SetValue(float64(bat.Percentage) / 100)
+	}
+
+	// Read the initial battery state.
+	bat, err := battery.Read()
+	updateUI(bat, err)
+
+	// Refresh the battery state periodically.
+	go func() {
+		ticker := time.NewTicker(10 * time.Second)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			bat, err := battery.Read()
+
+			// Fyne UI updates must happen on the Fyne call thread.
+			fyne.Do(func() {
+				updateUI(bat, err)
+			})
+		}
+	}()
+
 	window.ShowAndRun()
 }
